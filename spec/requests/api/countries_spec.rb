5996052710cf402456b02d094fae974fc547d73a
Carlos Gochez <cgotcha@gmail.com>
2021-09-21 22:45:57 -0600

    Add RSPEC

---
+++ spec/requests/api/countries_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Api::CountriesController", type: :request do
+  describe "GET /api/countries" do
+    it "works! (now write some real specs)" do
+      get api_countries_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "gets 404" do
+      get api_country_path(1)
+      expect(response).to have_http_status(404)
+    end
+
+    it "gets 200" do
+      country = Country.create(name: "Arandelle", code: "ARA")
+      get api_country_path(country.id)
+      expect(response).to have_http_status(200)
+    end
+
+    it "creates a new country" do
+      data = { data: { name: "Zarkovia", code: "ZAR" }}
+      post api_countries_path, params: data, headers: {"Content-Type" => "application/json"}, as: :json
+      expect(response).to have_http_status(201)
+      body = JSON.parse(response.body)
+      expect(body["data"]["name"]).to eq("Zarkovia")
+    end
+  end
+end
